8e7e73e311c6a879b31ff02bed16b283c9b22aa6
Lisec, George <glisec@iu.edu>
2025-03-24 10:38:00 -0400

    Editing colors, fonts, and spacing of headers

--- src/app/ui/sidebar/sidebar.tsx
+++ src/app/ui/sidebar/sidebar.tsx
@@ -43,15 +43,17 @@ export default function Sidebar() {
           key={"inactive"}
           className="flex flex-col flex-wrap lg:w-1/4 lg:h-screen bg-black items-end"
         >
-          <header className="flex lg:w-full justify-center p-4 ring h-1/12"></header>
+          <header className="flex lg:w-full justify-center p-4 h-1/12 text-white font-extralight items-end ">
+            Available: ✅ | Booked: 🚫
+          </header>
           <div className="flex flex-col lg:w-full h-10/12 p-6 space-y-4 overflow-scroll items-center">
             {roomData.map((room) => (
               <RoomCard key={room.room} room={room.room} times={room.times} />
             ))}
           </div>
-          <footer className="flex lg:w-full justify-center p-3 ring h-1/12">
+          <footer className="flex lg:w-full justify-center p-3 h-1/12">
             <button
-              className="flex lg:h-full justify-center items-center lg:w-1/2 rounded-lg hover:bg-white/10 hover:ring-2 hover:ring-white duration-300 p-4 shadow-2xl bg-gradient-to-tl from-white/10 to-white/20"
+              className="flex lg:h-full justify-center items-center lg:w-1/2 rounded-lg hover:bg-white/10 hover:ring hover:ring-white duration-300 p-4 shadow-2xl bg-gradient-to-tl from-white/10 to-white/20"
               onClick={(e) => {
                 e.preventDefault();
                 setSideBarVisibility(false);
@@ -80,14 +82,14 @@ export default function Sidebar() {
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           key={"active"}
-          className="flex flex-col flex-wrap lg:w-1/4 h-full bg-black"
+          className="flex flex-col flex-wrap lg:w-1/4 h-full bg-gradient-to-br from-black to-white/15 font-extralight"
         >
           <header className="flex lg:w-full h-2/12 justify-center p-4">
             <div className="flex justify-center items-center w-full rounded-lg bg-gradient-to-tl from-white/10 to-white/20">
-              <h1 className="text-white text-4xl font-mono">Nextroom</h1>
+              <h1 className="text-white text-4xl">Nextroom</h1>
             </div>
           </header>
-          <div className="flex flex-col lg:w-full h-9/12 justify-center font-mono p-4 space-y-4">
+          <div className="flex flex-col lg:w-full h-9/12 justify-center p-4 space-y-4">
             <div className="flex lg:w-full h-1/2  p-2">
               This will be a hero section.
             </div>
@@ -95,7 +97,7 @@ export default function Sidebar() {
           </div>
           <footer className="flex lg:w-full h-1/12 justify-center ring p-3">
             <button
-              className="flex justify-center items-center lg:w-1/2 rounded-lg hover:bg-white/10 hover:ring-2 hover:ring-white duration-300 p-4 shadow-2xl bg-gradient-to-tl from-white/10 to-white/20"
+              className="flex justify-center items-center lg:w-1/2 rounded-lg hover:bg-white/10 hover:ring hover:ring-white duration-300 p-4 shadow-2xl bg-gradient-to-tl from-white/10 to-white/20"
               onClick={(e) => {
                 e.preventDefault();
                 setSideBarVisibility(true);
